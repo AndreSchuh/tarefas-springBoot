a07c81d7b26f0c377c4cecf8e52d55fd9d3e61bf
André <andregabschuh@gmail.com>
2025-01-28 16:05:31 -0300

    Initial commit

---
+++ tarefas/src/main/java/com/mballem/tarefas/web/controller/ContatoController.java
@@ -0,0 +1,48 @@
+package com.mballem.tarefas.web.controller;
+
+import com.mballem.internal.entity.Contato;
+import com.mballem.internal.service.ContatoService;
+
+public class ContatoController {
+
+    // EXERCICIO 1
+    public Object create() {
+
+    }
+
+    // EXERCICIO 2
+    public Object getContatoById() {
+
+        return null;
+    }
+
+    // EXERCICIO 3
+    public Object getContatoByNome() {
+
+        return null;
+    }
+
+    // EXERCICIO 4
+    public Object getQuantidadeDeContatos() {
+
+        return null;
+    }
+
+    // EXERCICIO 5
+    public Object getContatosByDataNascimento() {
+
+        return null;
+    }
+
+    // EXERCICIO 6
+    public Object updateContatoById() {
+
+        return null;
+    }
+
+    // EXERCICIO 7
+    public Object deleteById() {
+
+        return null;
+    }
+}
