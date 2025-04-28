b65c805f4909aae172e3753436c50e8b55bbc4d3
andre <andregabschuh@gmail.com>
2025-04-27 21:11:59 -0300

    Realizando exercícios sobre métodos de atualização. buscas e deleção.

--- tarefas/src/main/java/com/mballem/tarefas/web/controller/ContatoController.java
+++ tarefas/src/main/java/com/mballem/tarefas/web/controller/ContatoController.java
@@ -2,10 +2,14 @@ package com.mballem.tarefas.web.controller;
 
 import com.mballem.internal.entity.Contato;
 import com.mballem.internal.service.ContatoService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas/contatos")
@@ -25,37 +29,44 @@ public class ContatoController {
     }
 
     // EXERCICIO 2
-    public Object getContatoById() {
-
-        return null;
+    @GetMapping("/getContatoById/{id}")
+    public ResponseEntity<Contato> getContatoById(@PathVariable Long id) {
+        Contato contato = contatoService.getById(id);
+        return ResponseEntity.ok(contato);
     }
 
     // EXERCICIO 3
-    public Object getContatoByNome() {
-        return null;
+    @GetMapping("/getContatoByNome/{nome}")
+    public ResponseEntity<Contato> getContatoByNome(@PathVariable String nome) {
+        Contato contato = contatoService.getContatoByNome(nome);
+        return ResponseEntity.ok(contato);
     }
 
     // EXERCICIO 4
-    public Object getQuantidadeDeContatos() {
-
-        return null;
+    @GetMapping("/getQuantidadeDeContatos")
+    public ResponseEntity<Integer> getQuantidadeDeContatos() {
+        int contatos = contatoService.getAll();
+        return ResponseEntity.ok(contatos);
     }
 
     // EXERCICIO 5
-    public Object getContatosByDataNascimento() {
-
-        return null;
+    @GetMapping("/getContatoByDataNascimento/{date}")
+    public ResponseEntity<List<Contato>> getContatosByDataNascimento(@PathVariable LocalDate date) {
+        List<Contato> contatos = contatoService.getByDataNascimento(date);
+        return ResponseEntity.ok(contatos);
     }
 
     // EXERCICIO 6
-    public Object updateContatoById() {
-
-        return null;
+    @PatchMapping("/updateContato/{id}")
+    public ResponseEntity<Contato> updateContatoById(@PathVariable Long id, @RequestBody Contato contatoAtualizado) {
+        Contato contato = contatoService.update(id, contatoAtualizado);
+        return ResponseEntity.ok(contato);
     }
 
     // EXERCICIO 7
-    public Object deleteById() {
-
-        return null;
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id) {
+        String resultado = contatoService.delete(id);
+        return ResponseEntity.ok(resultado);
     }
 }
